27387dc6239be54c96833d0f1cff57c6d12d91aa
Gen2zx <muhfazhanoyia@gmail.com>
2026-08-15 23:06:59 +0800

    laravel-project-update-2

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('register');
+    return view('authentication.register');
 });
 
 Route::post('/login-proses', 
